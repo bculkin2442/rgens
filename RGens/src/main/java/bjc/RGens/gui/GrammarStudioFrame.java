56b310cef98d5da34cbef37757a04e257561564a
bculkin2442 <bjculkin@mix.wvu.edu>
2016-08-29 10:40:33 -0400

    Started some work on a GUI app

--- RGens/src/main/java/bjc/RGens/gui/GrammarStudioFrame.java
+++ RGens/src/main/java/bjc/RGens/gui/GrammarStudioFrame.java
@@ -1,11 +1,30 @@
 package bjc.RGens.gui;
 
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+import bjc.utils.gui.layout.AutosizeLayout;
 
 public class GrammarStudioFrame extends JFrame {
 	private static final long serialVersionUID = 1476431892446002428L;
 
 	public GrammarStudioFrame() {
 		super("Grammar Studio");
+
+		setLayout(new AutosizeLayout());
+
+		JDesktopPane mainPane = new JDesktopPane();
+
+		JMenuBar menuBar = new JMenuBar();
+
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.setMnemonic('m');
+		
+		menuBar.add(fileMenu);
+
+		setJMenuBar(menuBar);
+		add(mainPane);
 	}
 }
