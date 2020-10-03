0a968a947bef835882967012a18f9e27cf949c60
Benjamin J. Culkin <bjculkin@mix.wvu.edu>
2020-10-03 09:15:43 -0300

    update

--- dotest.sh
+++ dotest.sh
@@ -2,5 +2,5 @@
 
 mvn clean compile exec:java > outp.txt 2> error.txt;
 
-tail -n +32 outp.txt | view -c "normal! zR" -c "tabe error.txt" -c "normal! gt" -;
+tail -n +28 outp.txt | view -c "normal! zR" -c "tabe error.txt" -c "normal! gt" -;
 # tail -n +32 outp.txt | view -c "normal! zR" -;
